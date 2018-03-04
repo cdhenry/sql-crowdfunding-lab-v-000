38bfa0e16912e2c69ddf32b7d9f6eeaba1109a59
Chris Henry <cdhenry@gmail.com>
2018-03-04 21:19:56 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,15 +2,16 @@ CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal INTEGER,
+  funding_goal REAL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE users(
-  name age
+  name TEXT,
+  age INTEGER
 );
 
 CREATE TABLE pledges(
-  ammount
+  ammount REAL
 );
