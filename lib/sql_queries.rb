2d36567002889519a74e67b7d2b27f1cb4fa0dd1
Chris Henry <cdhenry@gmail.com>
2018-03-04 23:11:21 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title"
+  "SELECT projects.title, SUM(pledges.amount) 
+    FROM projects 
+    INNER JOIN pledges 
+    ON projects.id = pledges.project_id 
+    GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
@@ -15,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id WHERE SUM(pledges.amount) - projects.funding_goal > 0"
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY project.title SUM(pledges.amount) - projects.funding_goal > 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
